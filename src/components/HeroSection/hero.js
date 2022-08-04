78e39b4eaba3bc9f1bd2af84734afa931f22c75c
okhot <arreyetta1jnr@gmail.com>
2022-08-04 23:30:43 +0100

    image bug not fixed yet

---
+++ src/components/HeroSection/hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const hero = () => {
+  return (
+    <div>
+     
+      <h5>Food App</h5>
+      <h1>Why stay hungry when you can order form Bella Onojie</h1>
+      <p>Download the bella onoje’s food app now on</p>
+    </div>
+  )
+}
+
+export default hero;
