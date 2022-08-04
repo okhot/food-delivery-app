cc4f33574676860010eff9404a6d73af4f22e843
okhot <arreyetta1jnr@gmail.com>
2022-08-04 23:34:12 +0100

    image bug not fixed yets

--- src/components/HeroSection/heroSection.jsx
+++ src/components/HeroSection/heroSection.jsx
@@ -9,7 +9,9 @@ export default function Hero() {
     <section className="Hero__section">
 
       <div className="background">
-      <video src="https://youtu.be/eOV2tx1aJZA"></video>
+      <video src="https://youtu.be/eOV2tx1aJZA">
+        bug fix
+      </video>
       </div>
 
     </section>
